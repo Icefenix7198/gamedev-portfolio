e728d35c83d8eeb62a41c456944e59baed817f69
Icefenix7198 <99949593+Icefenix7198@users.noreply.github.com>
2025-02-23 22:06:30 +0100

    Fix Twisted image.ts

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -65,7 +65,7 @@ export default [
 
     </div>
     `, "#5a78af",true,false),
-    new ProjectData("project-3", "Twisted Tent", "twistedTent.png", `
+    new ProjectData("project-3", "Twisted Tent", "img/projects/twistedTent.png", `
     <div class="paragraph">
         <strong>Twisted Tents</strong> is a small RPG made by a team of 9 people over the course of 4 months. In this short game you will follow a magician who tries to flee from a cursed wandering circus full of monster. 
         <br/><br/>
@@ -98,7 +98,7 @@ export default [
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
     </div>
-    `, "#383838"),
+    `, "#383838",false,true),
     new ProjectData("project-4", "Eugeneable", "img/projects/project-4-icon.png", `
     <div class="paragraph">
     <strong>Eugeneable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
