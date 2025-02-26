9fd35cafb3aff64b8de3d6e55f510174dedc7654
Icefenix7198 <99949593+Icefenix7198@users.noreply.github.com>
2025-02-26 13:41:31 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
-<template>
+i<template>
   <div class="footer">
     
     <div class="left"><a href="https://github.com/schouffy/gamedev-portfolio" target="blank">Portfolio</a> by schouffy</div>
-    <div class="right">Reach me at <a href="mailto:my@email.com">my@email.com</a> or <router-link to="/contact">through here</router-link></div>
+    <div class="right">Reach me at <a href="mailto:ericsb2003@gmail.com">ericsb2003@gmail.com/a> or <router-link to="/contact">through here</router-link></div>
   </div>
 </template>
 
@@ -23,7 +23,7 @@ export default Vue.extend({
   width: 100%;
   font-size: 0.8em;
   opacity: 0.7;
-  padding-bottom: 30px;
+  padding-bottom: 20px;
 }
 
 .left, .right {
