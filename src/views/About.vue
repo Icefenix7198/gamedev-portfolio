3d604375330a075a36f7060381310d9b420a1629
Icefenix7198 <99949593+Icefenix7198@users.noreply.github.com>
2025-02-26 23:00:58 +0100

    Update About.vue
    Fix spelling errors

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,12 +5,12 @@
     <div class="paragraph">
       <div>
         I'm a video game design and development student from Spain, primarily focused on the <strong>design</strong> side of development.<br/>
-        While my main interest lies in design, specially characther design and level design , I also enjoy the programing aspects of videogames, although I'm more lacking in this aspect.
+        While my main interest lies in game design, especially character and level design, I also enjoy the programming aspects of video games, although I'm lacking in this aspect.
       </div>
 
       <!--<div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">stuff</router-link>, on <router-link to="/other-projects">other stuff</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.</div> -->
 
-      <div style="margin-top: 40px;">Aside from video games, I am also passionate in martial arts, specifically judo, in which I achieved the rank of black belt, and board games, primary tabletop roleplaying games, as they allow to create stories with other people in an immersive way.</div>
+      <div style="margin-top: 40px;">Aside from video games, I am also passionate about martial arts, specifically judo, in which I achieved the rank of black belt, and board games, primarily tabletop roleplaying games, as they allow me to create stories with other people in an immersive way.</div>
     </div>
 
     <div class="photo">
